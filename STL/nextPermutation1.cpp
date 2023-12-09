a4b69bbfb8f7e1bbde2d0be3911f76c7431b6339
Ayush <ayush0999aryan@gmail.com>
2023-12-09 13:01:36 +0530

    nextPermutation-ex

---
+++ STL/nextPermutation1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+#include<utility>
+using namespace std;
+int main()
+{
+    string s = "1234";
+    int i =0;
+    sort(s.begin(),s.end());
+    do{
+        i++;
+    }while(next_permutation(s.begin(), s.end()));
+
+    cout << i << endl;
+
+
+}
